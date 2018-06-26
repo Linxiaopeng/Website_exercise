dcf980baf0b9d82ba85f48a7b9d7e71a95f59048
Linxiaopeng <Linxiaopeng@users.noreply.github.com>
2018-06-26 18:59:01 +0800

    basic exercise

--- JavaScript_basic.js
+++ JavaScript_basic.js
@@ -12,3 +12,7 @@ var day = today.getDay();
 var daylist = ["Sunday", "Monday", "Tuesday", "Wednesday ", "Thursday", "Friday", "Saturday"];
 console.log("Today is:" + daylist[day] + ".");
 
+var hour = today.getHours();
+var minute = today.getMinutes();
+var second = today.getMinutes();
+console.log("Current Time: " + hour + ": " + minute + ": " + second);
