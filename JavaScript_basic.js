9dcb9b3619af552710ab5711c041736c287cbbf9
Linxiaopeng <Linxiaopeng@users.noreply.github.com>
2018-06-27 11:47:31 +0800

    exercise 4

--- JavaScript_basic.js
+++ JavaScript_basic.js
@@ -40,3 +40,13 @@ if (mm < 10) {
 }
 
 console.log(dd + "-" + mm + "-" + yy);
+
+// 4. Write a JavaScript program to find the area of a triangle where lengths of the three of its sides are 5, 6, 7.
+
+function areaTriangle(side1, side2, side3) {
+    var perimeter = (side1 + side2 + side3) / 2;
+    var area = Math.sqrt(perimeter * (perimeter - side1) * (perimeter - side2) * (perimeter - side3));
+    return area;
+};
+
+console.log(areaTriangle(5, 6, 7));
