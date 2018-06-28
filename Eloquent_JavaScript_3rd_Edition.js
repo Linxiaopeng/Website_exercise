ab7d63d6852c5cc46aecc027b41a8587f9395fab
Linxiaopeng <linxiaopeng2013@gmail.com>
2018-06-28 23:14:19 +0800

    Chapter 3

--- Eloquent_JavaScript_3rd_Edition.js
+++ Eloquent_JavaScript_3rd_Edition.js
@@ -37,3 +37,55 @@ for (var y = 0; y < size; y++) {
 }
 
 console.log(board);
+
+// Chapter 3: Function
+
+// 3.1 Minimum
+
+function min(i, j) {
+    if (i < j)
+        return i;
+    else
+        return j;
+}
+
+console.log(min(0, 10));
+
+// 3.2 Recursion
+
+function checkEven(n) {
+    if (n > 0 && n % 2 == 0)
+        return n + " is even."
+    else if (n > 0 && n % 2 != 0)
+        return n + " is odd."
+    else
+        return n + " is a negative number."
+}
+
+console.log(checkEven(50));
+
+// 3.3 Bean counting
+
+function countBs(str) {
+    j = 0;
+    for (i = 0; i < str.length; i++) {
+        if (str.charAt(i) === "B") {
+            j++;
+        }
+    }
+    return j;
+}
+
+console.log(countBs("BBBBBC"));
+
+function countChar(str1, str2) {
+    j = 0;
+    for (i = 0; i < str1.length; i++) {
+        if (str1.charAt(i) === str2) {
+            j++;
+        }
+    }
+    return j;
+}
+
+console.log(countChar("BBBBBC", "C"));
