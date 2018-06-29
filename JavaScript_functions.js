e1ec604ecab040b529c91e57981ae483bdd10261
Linxiaopeng <Linxiaopeng@users.noreply.github.com>
2018-06-29 17:14:14 +0800

    exercise 1

--- JavaScript_functions.js
+++ JavaScript_functions.js
@@ -7,3 +7,15 @@ JavaScript functions [29 exercises with solution]
 // Example x = 32243;
 // Expected Output: 34223	
 
+function reverseNum(num) {
+    var j = '';
+    for (i = num.length; i > 0; i--) {
+        j = j + num.slice(0, i).slice(-1);
+    } return j;
+}
+
+console.log(reverseNum("JavaScript"));
+
+// 2. Write a JavaScript function that checks whether a passed string is palindrome or not ?
+
+
