8ed61e3133526634730840e639d1e593db6c1d97
Linxiaopeng <Linxiaopeng@users.noreply.github.com>
2018-06-29 19:25:00 +0800

    exercise 2 to 3

--- JavaScript_functions.js
+++ JavaScript_functions.js
@@ -18,4 +18,33 @@ console.log(reverseNum("JavaScript"));
 
 // 2. Write a JavaScript function that checks whether a passed string is palindrome or not ?
 
+function checkPal(str) {
+    if (str < 3) {
+        alert("Uh Oh");
+        return false;
+    }
+    str = str.toLowerCase().replace(/[^a-zA-Z0-9]+/g, '');
 
+    originalStr = str.split().join();
+    reverseStr = str.split().reverse().join();
+    return originalStr == reverseStr ? true : false;
+}
+
+console.log(checkPal("Madam"));
+
+// 3. Write a JavaScript function that generates all combinations of a string. 
+
+function generateCom(str) {
+    str = str.split("");
+    var strNew = "";
+    for (i = 0; i < str.length; i++) {
+        strNew = str[i];
+        console.log(strNew);
+        for (j = i + 1; j < str.length; j++) {
+            strNew += str[j];
+            console.log(strNew);
+        }
+    }
+}
+
+generateCom("Google");
