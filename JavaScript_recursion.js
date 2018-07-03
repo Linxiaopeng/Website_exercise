0154b8d7176657a3a69ed2fd2f6e5b59b35d4512
Linxiaopeng <Linxiaopeng@users.noreply.github.com>
2018-07-03 18:12:36 +0800

    exercise 9

--- JavaScript_recursion.js
+++ JavaScript_recursion.js
@@ -99,3 +99,24 @@ binoarySearch([13, 32, 19, 123, 56, 3327, 2, 6731], 6731);
 
 // 9. Write a merge sort program in JavaScript.
 
+Array.prototype.merge_Sort = function () {
+    if (this.length <= 1) {
+        return this;
+    }
+
+    var half = parseInt(this.length / 2);
+    var left = this.slice(0, half).merge_Sort();
+    var right = this.slice(half, this.length).merge_Sort();
+    var merge = function (left, right) {
+        var arry = [];
+        while (left.length > 0 && right.length > 0) {
+            arry.push((left[0] <= right[0]) ? left.shift() : right.shift());
+        }
+        return arry.concat(left).concat(right);
+    };
+
+    return merge(left, right);
+};
+
+var a = [34, 7, 23, 32, 5, 62];
+console.log(a.merge_Sort());
