357cb3adf0c938a27e7b2f3c3a7154702beaf6ba
Linxiaopeng <Linxiaopeng@users.noreply.github.com>
2018-07-03 16:35:50 +0800

    exercise 2 to 9

--- JavaScript_recursion.js
+++ JavaScript_recursion.js
@@ -12,17 +12,74 @@ function calculateFactorial(n) {
     return n * calculateFactorial(n - 1);
 }
 
+function calculateFactorial(n) {
+    return n === 0 ? 1 : n * calculateFactorial(n - 1);
+}
+
 calculateFactorial(5);
 
 // 2. Write a JavaScript program to find the greatest common divisor(gcd) of two positive numbers.
 
+// b !== 0 => !b output false
+// b == 0 => !b output ture
+
 function findGcd(a, b) {
-    while (b != 0) {
-        r = b;
-        b = a % b;
-        a = r;
+    if (!b) {
+        return a;
     }
-    return a;
+    return findGcd(b, a % b);
 }
 
-findGcd(12, 144);
+function findGcd(a, b) {
+    return b === 0 ? a : findGcd(b, a % b);
+}
+
+findGcd(13, 144);
+
+// 3. Write a JavaScript program to get the integers in range(x, y).
+
+function getIntergers(a, b) {
+    return a < b - 1 ? [a + 1, ...getIntergers(a + 1, b)] : [];
+}
+
+getIntergers(2, 9);
+
+// 4. Write a JavaScript program to compute the sum of an array of integers. 
+
+function calculateSum(a) {
+    return a.length == 1 ? a[0] : a[0] + calculateSum(a.slice(1, a.length));
+}
+
+calculateSum([1, 2, 3, 4, 5, 6]);
+
+// 5. Write a JavaScript program to compute the exponent of a number.
+
+function computExponent(a, b) {
+    return b / a == 1 ? 1 : 1 + computExponent(a, b / a);
+}
+
+computExponent(2, 1024);
+
+// 6. Write a JavaScript program to get the first n Fibonacci numbers. 
+
+function getFibonacci(n) {
+    if (n == 0) return [];
+    if (n == 1) return [0];
+    if (n == 2) return [0, 1];
+    pre = getFibonacci(n - 1);
+    return pre.concat(pre[n - 2] + pre[n - 3]);
+}
+
+getFibonacci(4);
+
+// 7. Write a JavaScript program to check whether a number is even or not. 
+
+function checkEven(a) {
+    a = Math.abs(a);
+    if (a == 0 || a == 2) return true;
+    if (a < 2) return false;
+    return checkEven(a - 2);
+}
+
+checkEven(-92)
+
