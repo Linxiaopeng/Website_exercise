021bff14ff3d72f6c38a0cece4fad7db056f0237
Linxiaopeng <linxiaopeng2013@gmail.com>
2018-07-04 23:30:17 +0800

    JavaScript Drawing Exercise 1

--- JavaScript_drawing.js
+++
@@ -1,7 +0,0 @@
-/*
-https://www.w3resource.com/javascript-exercises/javascript-drawing-exercises.php
-JavaScript drawing [6 exercises with solution]
-*/
-
-// 1. Write a JavaScript program to draw the following rectangular shape.
-
